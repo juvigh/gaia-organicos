0899a4bead85d1d9cf9047a27dfbbda6e11190a1
Julio <juliolk.4321@gmail.com>
2023-01-11 11:41:14 -0300

    validando a autenticação de usuário

--- src/pages/Product/EditProduct/EditProductPage.jsx
+++ src/pages/Product/EditProduct/EditProductPage.jsx
@@ -1,9 +1,34 @@
 import FormProduct from "../../../components/Forms/FormProduct/FormProduct";
 import Header from "../../../components/Header/Header";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 import { Container } from "../style";
 
 function EditProduct() {
+  const { id } = useParams();
+  const [user, setUser] = useState([]);
+  const userStorage = localStorage.getItem("user");
+  const userId = JSON.parse(userStorage);
+
+  async function getAnUser() {
+    const res = await axios.get(`http://localhost:5400/user/${userId}`);
+    setUser(res.data.user[0]);
+  }
+
+  if (!user) {
+    setTimeout(() => {
+      window.location.assign("http://127.0.0.1:5173/login");
+    }),
+      600;
+  }
+
+  useEffect(() => {
+    getAnUser();
+  }, [id]);
+
   return (
     <Container>
       <Header />
