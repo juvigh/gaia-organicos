ed0feb9f33238867cae3fe7d44c73b0332839047
Julio <juliolk.4321@gmail.com>
2023-01-16 21:30:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Products from "/src/pages/Product/ProductPage.jsx";
-import DetailsProduct from "/src/pages/Product/DetailsProduct/DetailsProductPage.jsx";
-import CreateProduct from "/src/pages/Product/CreateProduct/CreateProduct.jsx";
-import EditProduct from "/src/pages/Product/EditProduct/EditProductPage.jsx";
-import AuthUser from "/src/pages/User/AuthPage.jsx";
-import RegisterUser from "/src/pages/User/RegisterPage.jsx";
+import Products from "/src/pages/Product/ProductPage";
+import DetailsProduct from "/src/pages/Product/DetailsProduct/DetailsProductPage";
+import CreateProduct from "/src/pages/Product/CreateProduct/CreateProduct";
+import EditProduct from "/src/pages/Product/EditProduct/EditProductPage";
+import AuthUser from "/src/pages/User/AuthPage";
+import RegisterUser from "/src/pages/User/RegisterPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Products />} />
-        <Route path="product/:id" element={<DetailsProduct />} />
-        <Route path="register_product" element={<CreateProduct/>} />
-        <Route path="edit_product/:id" element={<EditProduct />} />
-        <Route path="register" element={<RegisterUser />} />
-        <Route path="login" element={<AuthUser />} />
+        <Route path="/product/:id" element={<DetailsProduct />} />
+        <Route path="/register_product" element={<CreateProduct/>} />
+        <Route path="/edit_product/:id" element={<EditProduct />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/login" element={<AuthUser />} />
       </Routes>
     </Router>
   );
